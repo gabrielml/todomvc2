58fd8f4375fac062bb2f1bf8e68839b75e19fff2
Gabriel Mijares <gabrielmljob@gmail.com>
2018-07-21 18:18:40 +0200

    refactor(js): improve code structure

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 function addNewTodo(keyPressEvent) {
     if(keyPressEvent.key === 'Enter'){
         const sourceInputElement = keyPressEvent.target;
-        const template = `
-            <input class="toggle" type="checkbox">
-            <label>${sourceInputElement.value}</label>
-            <button class="destroy"></button>
-        `;
-        const newTodoElement = document.createElement('li');
-        newTodoElement.innerHTML = template;
+        const newTodoElement = createTodoElement(sourceInputElement.value);
+
         document.querySelector('.todo-list').appendChild(newTodoElement);
         sourceInputElement.value = '';
     }
+}
+
+function createTodoElement(value){
+    const template = `
+            <input class="toggle" type="checkbox">
+            <label>${value}</label>
+            <button class="destroy"></button>
+        `;
+    const todoElement = document.createElement('li');
+
+    todoElement.innerHTML = template;
+
+    return todoElement;
 }
