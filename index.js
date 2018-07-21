a27b299a09ad4803061a9cf68b92efe7cac8d632
Gabriel Mijares <gabrielmljob@gmail.com>
2018-07-21 20:17:34 +0200

    feat(js): implement todo toggling support

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const todoList = [
-    {completed: false, description: 'Tarea1'},
-    {completed: true, description: 'Tarea2'}
+    {id: 0, completed: false, description: 'Tarea1'},
+    {id: 1, completed: true, description: 'Tarea2'}
 ];
+let nextId = todoList.length;
 document.addEventListener('DOMContentLoaded', update);
 
 
@@ -9,10 +10,17 @@ function addNewTodo(keyPressEvent) {
     if (keyPressEvent.key === 'Enter') {
         const sourceInputElement = keyPressEvent.target;
 
-        todoList.push({completed: false, description: sourceInputElement.value});
+        todoList.push({id: nextId++, completed: false, description: sourceInputElement.value});
         update();
         sourceInputElement.value = '';
     }
+}
+
+function toggleTodo(id) {
+    const todo = todoList.find(todo => todo.id === id);
+
+    todo.completed = !todo.completed;
+    update();
 }
 
 function update() {
@@ -20,19 +28,20 @@ function update() {
 
     todoListElement.innerHTML = null;
     todoList.forEach(todo => {
-        const newTodoElement = createTodoElement(todo.description);
+        const newTodoElement = createTodoElement(todo);
 
         todoListElement.appendChild(newTodoElement);
     });
 
-    function createTodoElement(value) {
+    function createTodoElement(todo) {
         const template = `
-            <input class="toggle" type="checkbox">
-            <label>${value}</label>
+            <input class="toggle" type="checkbox" ${todo.completed ? 'checked' : ''} onchange="toggleTodo(${todo.id})">
+            <label>${todo.description}</label>
             <button class="destroy"></button>
         `;
         const todoElement = document.createElement('li');
 
+        todoElement.className = todo.completed ? 'completed' : undefined;
         todoElement.innerHTML = template;
 
         return todoElement;
